fa7fb9d1a506cce9569dfb567f0806437b0bc83c
AIRTucha <airtucha@icloud.com>
2019-07-12 20:06:04 +0200

    Test for Thenable

---
+++ src/thenable.spec.ts
@@ -0,0 +1,59 @@
+import { expect } from 'chai'
+
+import { isThenable } from "./Thenable"
+import { Just, None } from './maybe'
+import { Success, Failure } from './result'
+
+describe("Thenable", () => {
+    describe("is", () => {
+        const isTrue = (obj: any) =>  expect(obj).to.be.true
+
+        it("Promise", () => {
+            isTrue(isThenable(Promise.resolve()))
+        })
+
+        it("Just", () => {
+            isTrue(isThenable(Just("")))
+        })
+
+        it("None", () => {
+            isTrue(isThenable(None()))
+        })
+
+        it("Success", () => {
+            isTrue(isThenable(Success("")))
+        })
+
+        it("Failure", () => {
+            isTrue(isThenable(Failure("")))
+        })
+
+        it("any.then()", () => {
+            isTrue(isThenable({ then: () => {}}))
+        })
+    })
+
+    describe("isn't", () => {
+        const isFalse = (obj: any) =>  expect(obj).to.be.false
+
+        it("object", () => {
+            isFalse(isThenable({}))
+        })
+
+        it("number", () => {
+            isFalse(isThenable(10))
+        })
+
+        it("boolean", () => {
+            isFalse(isThenable(true))
+        })
+
+        it("string", () => {
+            isFalse(isThenable("true"))
+        })
+
+        it("any.then === {}", () => {
+            isFalse(isThenable({ then: {} }))
+        })
+    })
+})
