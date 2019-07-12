330442a7d6d48baecf47c21ed9b762902e11c968
AIRTucha <airtucha@icloud.com>
2019-07-12 16:45:54 +0200

    Add Thenable facade around PromiseLike

---
+++ src/thenable.ts
@@ -0,0 +1,20 @@
+import { isFunction } from 'util'
+
+export interface Thenable<T> extends PromiseLike<T> {
+    /**
+     * Implementation of PromiseLike.then() for proper functioning of await
+     * @param onfulfilled Handler for fulfilled value
+     * @param onrejected Handler for onrejected value
+     * @return PromiseLike object which inclose new value
+     */
+    then<TResult1 = T, TResult2 = never>(
+        onfulfilled?: ( ( value: T ) => TResult1 | PromiseLike<TResult1> ) | undefined | null,
+        onrejected?: ( ( reason: any ) => TResult2 | PromiseLike<TResult2> ) | undefined | null
+    ): PromiseLike<TResult1 | TResult2>
+}
+
+/**
+ * @param obj Object which might be Thenable
+ * @returns Wether the object is Thenable
+ */
+export const isThenable = <T>(obj: any): obj is Thenable<T> => isFunction(obj.then)
